32f7547e2b3de139983e84d3f52422c68cfd092a
Itex34 <83979354+Itex34@users.noreply.github.com>
2025-03-16 13:29:31 +0100

    Update Model.hpp

--- Model.hpp
+++ Model.hpp
@@ -1,1 +1,54 @@
+#ifndef MODEL_HPP
+#define MODEL_HPP
+
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <glad/glad.h>
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    aiTextureType type;
+    std::string path;
+};
+
+class Mesh {
+public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+    unsigned int VAO, VBO, EBO;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void draw();
+};
+
+class Model {
+public:
+    std::vector<Texture> textures_loaded;
+    std::vector<Mesh> meshes;
+    std::string directory;
+
+    Model(const std::string& path);
+    void draw();
+
+private:
+    void loadModel(const std::string& path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, const aiScene* scene);
+    unsigned int TextureFromFile(const char* path, const std::string& directory);
+    unsigned int TextureFromAssimp(const aiTexture* aiTex);
+};
+
+#endif // MODEL_HPP
 
